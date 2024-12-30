5c4b5b48d4533009991134a4a13b8d1ce62b241c
workvivek <workvivek603@gmail.com>
2024-12-30 16:31:10 +0530

    CSS

--- solar_power/postcss.config.js
+++ solar_power/postcss.config.js
@@ -1,6 +1,18 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+module.exports = {
+  plugins: [
+    require("tailwindcss"), // Add Tailwind CSS plugin
+    require("autoprefixer"), // Add Autoprefixer plugin for vendor prefixes
+    process.env.NODE_ENV === "production"
+      ? require("@fullhuman/postcss-purgecss")({
+          // Optional, for production builds
+          content: [
+            "./src/**/*.html", // Specify where your HTML files are located
+            "./src/**/*.js", // Include JS files (or other frameworks like React, Vue)
+            "./src/**/*.jsx", // If you're using React JSX
+            "./src/**/*.tsx", // If you're using TypeScript
+          ],
+          safelist: [], // Optional: List of classes to never purge
+        })
+      : undefined,
+  ],
+};
