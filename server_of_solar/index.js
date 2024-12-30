2116cd270bbb1d90f70f6908ec3eb449aab5e638
workvivek <workvivek603@gmail.com>
2024-12-30 16:12:09 +0530

    origin

--- server_of_solar/index.js
+++ server_of_solar/index.js
@@ -1,39 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const authRouter = require('./router/auth-router');
-const contactRouter = require('./router/contact-router');
-const connectDB = require('./utils/db');
-const { errorMiddleware, loginErrorMiddleware } = require('./middlewares/erorr-middlewarers');
-const reviewRouter = require('./router/review-router');
-const adminRouter = require('./router/admin-router');
+const authRouter = require("./router/auth-router");
+const contactRouter = require("./router/contact-router");
+const connectDB = require("./utils/db");
+const {
+  errorMiddleware,
+  loginErrorMiddleware,
+} = require("./middlewares/erorr-middlewarers");
+const reviewRouter = require("./router/review-router");
+const adminRouter = require("./router/admin-router");
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
-    credentials: true
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "X-Auth-Token", "Origin"],
+  optionsSuccessStatus: 200,
 };
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
 // Routes
-app.use('/auth', authRouter);
-app.use('/form', contactRouter);
-app.use('/review', reviewRouter);
-app.use('/admin', adminRouter);
+app.use("/auth", authRouter);
+app.use("/form", contactRouter);
+app.use("/review", reviewRouter);
+app.use("/admin", adminRouter);
 // Error handling middleware
 app.use(errorMiddleware);
 app.use(loginErrorMiddleware);
 
-app.get('/', (req, res) => {
-    res.send('Hello Power of Solar');
+app.get("/", (req, res) => {
+  res.send("Hello Power of Solar");
 });
 
 // Start server
 const PORT = 3000;
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
